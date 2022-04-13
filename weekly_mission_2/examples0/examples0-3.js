0d1fb0ef50fd88fb94893882d567919c8b1633a1
cesarmezab <cmeza8599@gmail.com>
2022-04-13 15:37:52 -0700

    Actualización de ejemplo 3 de examples_0.js

--- weekly_mission_2/examples0/examples0-3.js
+++ weekly_mission_2/examples0/examples0-3.js
@@ -1,16 +1,15 @@
 // Ejemplo 3: Objeto con diferentes propiedades
 const myObject3 = {
-    name: "Tulio",
+    name: "Tobey",
     age: 2,
     nicknames: [
-     "Tulipan",
-      "Tulancingo",
-      "Tulish"
+     "Tobi",
+      "Firulais",
     ],
     address: {
       zip_code: "10000",
-      street: "Dr. Vertiz 11 Benito Juarez",
-      city: "CDMX"
+      street: "Dr. Un lugar aleatorio",
+      city: "BC"
     }
   }
   console.log("Ejemplo 3: Objeto con diferentes propiedades")

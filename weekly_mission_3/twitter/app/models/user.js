2ac2c644190a68ec679b638c1625c7bc4eca66b8
cesarmezab <cmeza8599@gmail.com>
2022-04-21 14:15:08 -0700

    Exercise 3 twiter: Add get methods to user in test and class

--- weekly_mission_3/twitter/app/models/user.js
+++ weekly_mission_3/twitter/app/models/user.js
@@ -1,13 +1,30 @@
 class User {
-    constructor(id,userName,name,bio,dateCreated,lastUpdated){
+    constructor(id,userName,name,bio){
         this.id=id
         this.userName= userName
         this.name = name
         this.bio = bio
         this.dateCreated = new Date()
         this.lastUpdated = new Date()
-
     }
+
+    get getUserName(){
+       return this.userName
+    }
+
+    get getBio(){
+        return this.bio
+    }
+
+    get getDateCreated(){
+        return this.dateCreated
+    }
+
+    get getLastUpdated(){
+        return this.lastUpdated
+    }
+
+       
 }
 
 module.exports = User
--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -11,4 +11,13 @@ describe("Esto es una suite de pruebas", () => {
       expect(user.dateCreated).not.toBeUndefined();
       expect(user.lastUpdated).not.toBeUndefined();
     });
+
+    test('Add getters', () => {
+      const user = new User(1,"carlogilmar","Carlo","Bio") 
+      
+      expect(user.getUserName).toBe("carlogilmar")
+      expect(user.getBio).toBe("Bio")
+      expect(user.getDateCreated).not.toBeUndefined();
+      expect(user.getLastUpdated).not.toBeUndefined();
+    });
   })

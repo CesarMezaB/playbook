cdd264775a4fa4410f87cb8fcb246e5729f44852
cesarmezab <cmeza8599@gmail.com>
2022-04-21 12:47:58 -0700

    Exercise 3 twiter: Add creation date requirement when creating an object

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -2,13 +2,13 @@ const User = require('./../../app/models/user')
 
 describe("Esto es una suite de pruebas", () => {
     test('Create an User class', () => {
-      const user = new User(1,"carlogilmar","Carlo","Bio","dateCreated","lastUpdated") 
+      const user = new User(1,"carlogilmar","Carlo","Bio") 
       
       expect(user.id).toBe(1);
       expect(user.userName).toBe("carlogilmar");
       expect(user.name).toBe("Carlo");
       expect(user.bio).toBe("Bio");
-      expect(user.dateCreated).toBe("dateCreated");
-      expect(user.lastUpdated).toBe("lastUpdated");
+      expect(user.dateCreated).not.toBeUndefined();
+      expect(user.lastUpdated).not.toBeUndefined();
     });
   })

21fd69147be3a39a83c44b06016e1a49f867b788
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-04-23 16:08:36 -0700

    Add facebok bio class

---
+++ weekly_mission_2/exercies/exercise-1/facebook/biography.js
@@ -0,0 +1,12 @@
+const biography ={
+    userName:"Cesar Meza",
+    totalPosts: 135,
+    generalInformation:{
+       city:"tijuana",
+        conctact:"cesar_meza017@hotmail.com"
+    },
+    amigos:200,
+    photos:15,
+    videos: 12,
+    RecordOfVisits:3
+}

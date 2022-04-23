2ac136bb0f6a90b2e48b64d50877f65aaa613896
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-04-22 17:35:26 -0700

    requirement 4 : showing the names of the users

--- weekly_mission_3/twitter/app/services/UserService.js
+++ weekly_mission_3/twitter/app/services/UserService.js
@@ -7,7 +7,22 @@ class UserService {
 
    static getInfo(user){
         return Object.values(user)
-   
+    }
+
+    static updateUserUserName(user,name){
+      
+        return user.userName = name
+    }
+
+    static getAllUserNames(...users){
+        let name1 = users[0][0].userName;
+        let name2 = users[0][1].userName;
+        let name3 = users[0][2].userName;
+         let arr =[name1,name2,name3]
+             
+              
+       
+        return arr
     }
 
 }
--- weekly_mission_3/twitter/test/services/UserService.test.js
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -19,4 +19,20 @@ describe("Tests for UserService",() =>{
        
     })
 
+    test("3. Update userName", () =>{
+        const user = UserService.create(1,"carlogilmar","Carlo")
+        UserService.updateUserUserName(user,"carlog")
+        expect(user.userName).toBe("carlog")
+    })
+
+    test("4. Given a list of users give me the list of UserNames", () =>{
+        const user1 = UserService.create(1, "carlogilmar1","Carlo")
+        const user2 = UserService.create(1, "carlogilmar2","Carlo")
+        const user3 = UserService.create(1, "carlogilmar3","Carlo")
+        const userNames = UserService.getAllUserNames([user1,user2,user3])
+        expect(userNames).toContain("carlogilmar1")
+        expect(userNames).toContain("carlogilmar2")
+        expect(userNames).toContain("carlogilmar3")
+    })
+
 })
